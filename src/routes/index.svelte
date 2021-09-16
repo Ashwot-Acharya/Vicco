f730f4485077b504aa36a825da229710e8e4de36
Ashwot-Acharya <ashwot1133@gmail.com>
2021-09-16 22:28:42 +0545

    Front page added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <div class ="jumbotron text-center "> <h1>VICCO: Future lies in your hand</h1> 
+    <h3><a class="" href="instagram.com" ><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        <a class=" " href="facebook.com" ><i class="fab fa-facebook"></i> </a>
+        <a class="" href="twitter.com"><i class="fab fa-twitter"></i></a>
+    </h3>
+    </div>
+    <div class="border text-white bg-primary">
+        Home
+        Login 
+        comments
+    </div>
+    <div class="container my-3 border p-3 text-white bg-dark">
+        <h3> About Vicco </h3>
+        <p>Vicco is a online blog template made from svelte which gives you an option to add a new article to your website with having to code even a little bit . It gives yoou the freedom of fully customiziable website with little effort </p>
+    </div>
+    <div class ="footer"></div>
+</div>
