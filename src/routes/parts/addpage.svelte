3abad126324307f4a71ab53482d0c647fee9cd3d
Ashwot-Acharya <ashwot1133@gmail.com>
2021-10-14 23:11:57 +0545

    Page adding part created

---
+++ src/routes/parts/addpage.svelte
@@ -0,0 +1,7 @@
+<div>
+    <label for="title">Title:</label>
+    <textarea style="height: 500px; width:500px;" ></textarea>
+    <label for="picture">Picture</label>
+    <input type="image" alt="piccture">
+    
+</div>
